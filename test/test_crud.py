0bc3d4b86f9d64fcc0ea465a4550bffebc54b8cc
kadimk4 <khaydarshin2007@gmail.com>
2024-01-25 04:29:23 +0300

    fix

--- test/test_crud.py
+++ test/test_crud.py
@@ -115,7 +115,6 @@ async def test_post_dish(ac: AsyncClient):
     })
     assert response.status_code == 201
     dish_id = response.json()['id']
-    print(dish_id)
     response = await ac.get(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/')
     assert len(response.json()) == 1
 
@@ -147,6 +146,9 @@ async def test_patch_dish(ac: AsyncClient):
     assert response.json()['description'] == "My updated dish description 1"
     assert response.json()['price'] == "14.50"
 
+
+
+# DELETE OPERATIONS
 @pytest.mark.asyncio
 async def test_delete_submenu(ac: AsyncClient):
     global menu_id
@@ -170,3 +172,46 @@ async def test_delete_menu(ac: AsyncClient):
     assert len(response.json()) == 0
 
 
+# ** Реализовать тестовый сценарий «Проверка кол-ва блюд и подменю в меню» из Postman с помощью pytest
+
+
+@pytest.mark.asyncio
+async def test_detailed_menu(ac: AsyncClient):
+    global menu_id
+    global submenu_id
+    menu = await ac.post(f'/api/v1/menus/', json={
+        "title": "My meny 1",
+        "description": "My menu description 1"
+    })
+    menu_id = menu.json()['id']
+    assert menu.status_code == 201
+    submenu = await ac.post(f'/api/v1/menus/{menu_id}/submenus/', json={
+        "title": "My submenu 1",
+        "description": "Mu submenu description 2"
+    })
+    assert submenu.status_code == 201
+    submenu_id = submenu.json()['id']
+    dish_1 = await ac.post(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/', json={
+        "title": "My dish 1",
+        "description": "My dish description 1",
+        "price": "12.50"
+    })
+    assert dish_1.status_code == 201
+    dish_2 = await ac.post(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/', json={
+        "title": "My dish 2",
+        "description": "My dish description 2",
+        "price": "13.50"
+    })
+    assert dish_2.status_code == 201
+    response = await ac.get(f'/api/v1/menus/{menu_id}')
+    assert response.status_code == 200
+    assert response.json()['submenus_count'] == 1
+    assert response.json()['dishes_count'] == 2
+    await ac.delete(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}')
+    check_submenu = await ac.get(f'/api/v1/menus/{menu_id}/submenus/')
+    assert check_submenu.json() == []
+    check_dish = await ac.get(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/')
+    assert check_dish.json() == []
+    await ac.delete(f'/api/v1/menus/{menu_id}')
+    check_menu = await ac.get(f'/api/v1/menus/')
+    assert check_menu.json() == []
